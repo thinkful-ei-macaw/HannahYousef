72c0cf16755d54da9e363a6e3a5a12f9491ed04f
Hannah Thorpe <hthorpe@protonmail.com>
2020-02-10 17:23:29 -0500

    solved problem2

--- set2.js
+++ set2.js
@@ -1,26 +1,19 @@
+'use strict';
 function jediName (firstName, lastName){
-    return lastName.slice(0,3)+firstName.slice(0,2);
+  return lastName.slice(0,3)+firstName.slice(0,2);
 }
 function beyond(num){
-    if (num === Infinity || -Infinity){
-       return 'And beyond';
-    }
-    else if (num === 0){
-        return 'Staying home';
-      }
-    else if (num % 2 === 0){
-        return 'To infinity';
-   }
-    else (num % 2 !== 0){
-        return 'To negative infinity';
-    }
+  if (num === Infinity || num === -Infinity){
+    return 'And beyond';
+  }
+  else if (num === 0){
+    return 'Staying home';
+  }
+  else if (num % 2 === 0){
+    return 'To infinity';
+  }
+  else if (num % 2 !== 0){
+    return 'To negative infinity';
+  }
 }
-console.log(beyond(0));
-function decode(string){
-   let message='';
-    for(let i=0;i<string.length;i++){
-        if (string[i] === 'a'){
-            message = string[2];
-        }
-    };
-}
+console.log(beyond(5));
