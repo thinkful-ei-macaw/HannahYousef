b843791ad7330308b4657c79513d06b51169b6fb
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-02-11 09:22:19 -0500

    new changes

--- set1.js
+++ set1.js
@@ -1,11 +1,21 @@
 function createGreeting(name, age){
-    let yearOfBirth = 2020 - age
+    let yearOfBirth = (2020 - age)
     return `I was born ${yearOfBirth}`
     
 
 }
 
+function getYearOfBirth(age) {
+    return 2020 - age
+}
+
 const firstGreeting = createGreeting("yousef", 26);
 console.log(firstGreeting)
 
-//hello
+function yearOfBirth(age) {
+    if (-20) {
+        throw new Error ("Age can not be negative");
+
+        return 
+    }
+}
